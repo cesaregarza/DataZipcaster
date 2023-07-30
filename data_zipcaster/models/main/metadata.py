e52fc74c2d1daeea6b83edc2e5a9dfee3dd01a68
Cesar E Garza <cesar@cegarza.com>
2023-07-30 16:35:25 -0500

    docstrings to the models

--- data_zipcaster/models/main/metadata.py
+++ data_zipcaster/models/main/metadata.py
@@ -13,6 +13,21 @@ __all__ = [
 
 
 class AnarchyMetadata(BaseModel):
+    """The model for metadata in an Anarchy match.
+
+    Fields:
+        - rank_before (str): The rank before the match.
+        - rank_after (str): The rank after the match.
+        - rank_before_s_plus (Optional[int]): The S+ rank before the match. Only
+            used if the rank is S+.
+        - rank_after_s_plus (Optional[int]): The S+ rank after the match. Only
+            used if the rank is S+.
+        - rank_exp_change (Optional[int]): The change in rank experience points
+            after the match. Not present in the middle of a series.
+        - is_rank_up (Optional[bool]): Whether the series was a rank up series.
+            Does not indicate the player ranked up after the match.
+    """
+
     rank_before: str
     rank_after: str
     rank_before_s_plus: Optional[int] = None
@@ -34,15 +49,59 @@ class AnarchyMetadata(BaseModel):
 
 
 class AnarchySeriesMetadata(AnarchyMetadata):
+    """The model for metadata in an Anarchy series. Contains all fields from
+    AnarchyMetadata and adds series win/loss counts.
+
+    Fields:
+        - rank_before (str): The rank before the match.
+        - rank_after (str): The rank after the match.
+        - rank_before_s_plus (Optional[int]): The S+ rank before the match. Only
+            used if the rank is S+.
+        - rank_after_s_plus (Optional[int]): The S+ rank after the match. Only
+            used if the rank is S+.
+        - rank_exp_change (Optional[int]): The change in rank experience points
+            after the match. Not present in the middle of a series.
+        - is_rank_up (Optional[bool]): Whether the series was a rank up series.
+            Does not indicate the player ranked up after the match.
+        - series_win_count (int): The number of wins in the series so far.
+        - series_lose_count (int): The number of losses in the series so far.
+    """
+
     series_win_count: int
     series_lose_count: int
 
 
 class AnarchyOpenMetadata(AnarchyMetadata):
+    """The model for metadata in an Anarchy match.
+
+    Fields:
+        - rank_before (str): The rank before the match.
+        - rank_after (str): The rank after the match.
+        - rank_before_s_plus (Optional[int]): The S+ rank before the match. Only
+            used if the rank is S+.
+        - rank_after_s_plus (Optional[int]): The S+ rank after the match. Only
+            used if the rank is S+.
+        - rank_exp_change (Optional[int]): The change in rank experience points
+            after the match. Not present in the middle of a series.
+        - is_rank_up (Optional[bool]): Whether the series was a rank up series.
+            Does not indicate the player ranked up after the match.
+    """
+
     pass
 
 
 class XMetadata(BaseModel):
+    """The model for metadata in an Xbattles match.
+
+    Fields:
+        - x_power_after (Optional[float]): The X Power after the match. Only
+            present in the last match of a series.
+        - rank_estimate (Optional[int]): The estimated rank after the match.
+            Only present for Top 3000 players.
+        - series_win_count (int): The number of wins in the series so far.
+        - series_lose_count (int): The number of losses in the series so far.
+    """
+
     x_power_after: Optional[float] = None
     rank_estimate: Optional[int] = None
     series_win_count: int
--- data_zipcaster/models/main/vs_modes.py
+++ data_zipcaster/models/main/vs_modes.py
@@ -25,11 +25,29 @@ __all__ = [
 
 
 class Awards(BaseModel):
+    """The model for awards in a vs mode match.
+
+    Fields:
+        - name (str): The name of the award.
+        - rank (AwardRankType): The rank of the award. Either "gold" or "silver"
+    """
+
     name: str
     rank: AwardRankType
 
 
 class TeamResult(BaseModel):
+    """The result of a team in a vs mode match.
+
+    Fields:
+        - paint_ratio (Optional[float]): The paint ratio of the team.
+        - score (Optional[int]): The score of the team.
+        - noroshi (Optional[int]): How many Signals the team obtained. Only used
+            during Splatfest.
+        - team_result (ResultType): The result of the team, known as the
+            judgement.
+    """
+
     paint_ratio: Optional[float] = None
     score: Optional[int] = None
     noroshi: Optional[int] = None
@@ -37,6 +55,18 @@ class TeamResult(BaseModel):
 
 
 class SplatfestTeam(BaseModel):
+    """The Splatfest team data of a team in a vs mode match.
+
+    Fields:
+        - team_name (str): The name of the team.
+        - synergy_bonus (Optional[float]): The synergy bonus value from the
+            team's gear synergy.
+        - synergy_name (Optional[str]): The name of the gear synergies the team
+            has.
+        - tricolor_role (Optional[TricolorRoleType]): The tricolor role of the
+            team.
+    """
+
     team_name: str
     synergy_bonus: Optional[float] = None
     synergy_name: Optional[str] = None
@@ -44,6 +74,18 @@ class SplatfestTeam(BaseModel):
 
 
 class Team(BaseModel):
+    """The data of a team in a vs mode match.
+
+    Fields:
+        - players (list[Player]): The players in the team.
+        - color (str): The color of the team. A hex color code, prepended by a
+            "#".
+        - order (int): The order of the team, 1-indexed.
+        - result (Optional[TeamResult]): The result of the team.
+        - splatfest (Optional[SplatfestTeam]): The Splatfest team data of the
+            team.
+    """
+
     players: list[Player]
     color: str
     order: int
@@ -52,12 +94,44 @@ class Team(BaseModel):
 
 
 class SplatfestMetadata(BaseModel):
+    """The metadata of a Splatfest match.
+
+    Fields:
+        - match_multiplier (int): The match multiplier of the Splatfest match.
+        - clout (int): The clout earned in the Splatfest match.
+        - jewel (int): Not sure what this is, but it's in the data.
+    """
+
     match_multiplier: int
     clout: int
     jewel: int
 
 
 class VsExtract(BaseModel):
+    """The main model for vs mode matches.
+
+    Fields:
+        - knockout (KnockoutType): The knockout status of the match.
+        - mode (ModeType): The mode of the match.
+        - result (ResultType): The result of the match.
+        - rule (RuleType): The rule of the match.
+        - stage (str): The stage of the match. Is the stage's ID, everything
+            after "VsStage-" in the base64-decoded stage ID.
+        - start_time (dt.datetime): The start time of the match.
+        - duration (dt.timedelta): The duration of the match.
+        - teams (list[Team]): The teams of the match.
+        - awards (list[Awards]): The awards of the match.
+        - id (str): The ID of the match. Is the base64-encoded battle ID.
+        - series_metadata (Optional[AnarchyMetadata | XMetadata]): The metadata
+            of the series, if the match is part of a series.
+        - match_power (Optional[float]): The match power of the match, if the
+            match is part of a series.
+        - challenge_id (Optional[str]): The challenge ID of the match, if the
+            match is part of a challenge.
+        - splatfest_metadata (Optional[SplatfestMetadata]): The metadata of the
+            Splatfest, if the match is part of a Splatfest.
+    """
+
     knockout: KnockoutType
     mode: ModeType
     result: ResultType
